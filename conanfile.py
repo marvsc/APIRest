b00bd0c4fcf666a73520232b812148e46c5b3e1e
marcus <marcus@terra>
2026-03-29 19:24:02 -0300

    Correção

--- conanfile.py
+++ conanfile.py
@@ -47,6 +47,7 @@ class apirestRecipe(ConanFile):
         self.options["poco"].enable_xml = False
         self.options["poco"].enable_zip = False
         self.options["poco"].enable_prometheus = False
+        self.options["openssl"].no_module = True
 
     def layout(self):
         cmake_layout(self)
--- src/APIRestServerApplication.cpp
+++ src/APIRestServerApplication.cpp
@@ -62,6 +62,10 @@ int APIRestServerApplication::main(const std::vector<std::string> &args) {
         return Poco::Util::Application::EXIT_CONFIG;
     }
     std::ifstream pkcs12_stream(config().getString(Configuration::APIRestConfigurationKeys::APIREST_PKCS12_PATH), std::ios::binary);
+    if (!pkcs12_stream.good()) {
+        logger().error("Certificado não encontrado em %s", config().getString(Configuration::APIRestConfigurationKeys::APIREST_PKCS12_PATH));
+        return Poco::Util::Application::EXIT_CONFIG;
+    }
     std::unique_ptr<Poco::Crypto::PKCS12Container> container(nullptr);
     if (config().has(Configuration::APIRestConfigurationKeys::APIREST_PKCS12_PASSWORD_AES_256_CBC_BASE64)
             && config().has(Configuration::APIRestConfigurationKeys::APIREST_PKCS12_PASSWORD_AES_KEY)
