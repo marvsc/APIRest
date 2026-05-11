d83fe1056b157069a500d0012f4dacb0693cb324
marcus.chaves <marcus.chaves@192.168.170.44>
2026-05-11 17:28:55 -0300

    WIP

--- test_package/src/UnitTests/src/APIRestServerTest.cpp
+++ test_package/src/UnitTests/src/APIRestServerTest.cpp
@@ -10,8 +10,22 @@
 #include <Poco/Net/HTTPServerRequestImpl.h>
 #include <Poco/Net/FilePartSource.h>
 #include <Poco/Net/HTTPServerResponseImpl.h>
+#include <Poco/Net/HTTPRequestHandler.h>
 
 #include "../../../../src/APIRestRequestHandlerFactory.h"
+
+#define BUFFER_SIZE 256
+
+#define PRINTF_MESSAGE(format, ...) \
+    ([&]() { \
+            char buffer[BUFFER_SIZE]; \
+            std::snprintf(buffer, BUFFER_SIZE, format, ##__VA_ARGS__); \
+            return std::string(buffer); \
+    }())
+
+#define CONTENT_TYPE_PLAIN_TEXT "text/plain; charset=utf-8";
+
+CPPUNIT_TEST_SUITE_REGISTRATION(APIRestServerTest);
 
 void APIRestServerTest::teste_rota_signature() {
     Poco::Net::HTTPServerRequestImpl request;
@@ -24,6 +38,10 @@ void APIRestServerTest::teste_rota_signature() {
     form.prepareSubmit(request);
     Poco::Net::HTTPServerResponseImpl response;
     APIRestRequestHandlerFactory factory("upload");
-    factory.createRequestHandler(request);
+    std::unique_ptr<Poco::Net::HTTPRequestHandler> handler(factory.createRequestHandler(request));
+    handler->handleRequest(request, response);
+    CPPUNIT_ASSERT_MESSAGE(PRINTF_MESSAGE("Status [%d], Reason [%s]", response.getStatus(), response.getReason().c_str()), response.getStatus() == Poco::Net::HTTPResponse::HTTP_OK);
+    CPPUNIT_ASSERT_MESSAGE(PRINTF_MESSAGE("Content type %s incorreto", response.getContentType().c_str()), response.getContentType() == CONTENT_TYPE_PLAIN_TEXT);
+    CPPUNIT_ASSERT_MESSAGE("Não retornou o conteúdo da assinatura em base 64", response.getContentLength() > 0);
 }
 
