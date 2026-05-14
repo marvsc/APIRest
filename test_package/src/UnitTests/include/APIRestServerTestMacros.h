808f920670c5042103b13f03cd955838a29a7a4c
Marcus Chaves <marcus.chaves@digitro.com>
2026-05-14 14:11:54 -0300

    Evolução

---
+++ test_package/src/UnitTests/include/APIRestServerTestMacros.h
@@ -0,0 +1,33 @@
+/*
+ * APIRestServerTestMacros.h
+ *
+ *  Created on: 14 de mai. de 2026
+ *      Author: marcus.chaves
+ */
+
+#ifndef TEST_PACKAGE_SRC_UNITTESTS_INCLUDE_APIRESTSERVERTESTMACROS_H_
+#define TEST_PACKAGE_SRC_UNITTESTS_INCLUDE_APIRESTSERVERTESTMACROS_H_
+
+#define BUFFER_LENGTH 256
+#define LOCAL_IP "127.0.0.1"
+#define DEFAULT_PORT 9000
+#define SIGNATURE_PATH "/signature"
+#define UPLOAD_PATH "."
+#define FILE_TO_ASSIGN "File-To-Assign"
+#define FILE_TO_ASSIGN_PATH "doc.txt"
+#define CERTIFICATE "Certificate"
+#define CERTIFICATE_PATH "certificado_teste_hub.pfx"
+#define PASSWORD "Password"
+#define AES_KEY "b90XmFVR51L485rxXXCRhupVxva0yDFh"
+#define AES_INITIALIZATION_VECTOR "NSVmgGXSm2jRTiyq"
+#define PKCS12_ENVVAR_PASSWORD "PKCS12_ENVVAR_PASSWORD"
+#define CONTENT_TYPE_PLAIN_TEXT "text/plain; charset=utf-8"
+
+#define PRINTF_MESSAGE(format, ...) \
+    ([&]() { \
+            char buffer[BUFFER_LENGTH]; \
+            std::snprintf(buffer, BUFFER_LENGTH, format, ##__VA_ARGS__); \
+            return std::string(buffer); \
+    }())
+
+#endif /* TEST_PACKAGE_SRC_UNITTESTS_INCLUDE_APIRESTSERVERTESTMACROS_H_ */
--- test_package/src/UnitTests/src/APIRestServerTest.cpp
+++ test_package/src/UnitTests/src/APIRestServerTest.cpp
@@ -7,73 +7,47 @@
 
 #include "../include/APIRestServerTest.h"
 
-#include <Poco/Net/HTTPServerRequestImpl.h>
+#include "../include/APIRestServerTestMacros.h"
+
+#include <OpenSSLUtils.h>
+
 #include <Poco/Net/FilePartSource.h>
-#include <Poco/Net/HTTPServerResponseImpl.h>
-#include <Poco/Net/HTTPRequestHandler.h>
-#include <Poco/Net/StreamSocket.h>
-#include <Poco/Net/HTTPServerSession.h>
-#include <Poco/Net/SocketAddress.h>
 #include <Poco/Net/ServerSocket.h>
 #include <Poco/Net/HTTPServer.h>
 #include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
 
 #include "../../../../src/APIRestRequestHandlerFactory.h"
-
-#define BUFFER_SIZE 256
-#define LOCAL_IP "127.0.0.1"
-#define DEFAULT_PORT 9000
-
-#define PRINTF_MESSAGE(format, ...) \
-    ([&]() { \
-            char buffer[BUFFER_SIZE]; \
-            std::snprintf(buffer, BUFFER_SIZE, format, ##__VA_ARGS__); \
-            return std::string(buffer); \
-    }())
-
-#define CONTENT_TYPE_PLAIN_TEXT "text/plain; charset=utf-8"
 
 CPPUNIT_TEST_SUITE_REGISTRATION(APIRestServerTest);
 
 void APIRestServerTest::teste_rota_signature() {
-    // TODO:
-    // Instanciar o request handler factory
-    Poco::Net::HTTPRequestHandlerFactory::Ptr router(new APIRestRequestHandlerFactory("."));
-    // Instanciar o http server
+    Poco::Net::HTTPRequestHandlerFactory::Ptr router(new APIRestRequestHandlerFactory(UPLOAD_PATH));
     Poco::Net::HTTPServerParams::Ptr http_server_params = new Poco::Net::HTTPServerParams;
-    // Iniciar o http server
-    Poco::Net::HTTPServer http_server(router, Poco::Net::ServerSocket(Poco::UInt16(DEFAULT_PORT)), http_server_params);
-    // Instanciar o http client session
+    Poco::Net::HTTPServer http_server(router, Poco::Net::ServerSocket(Poco::UInt16(DEFAULT_PORT)),
+            http_server_params);
     Poco::Net::HTTPClientSession session(LOCAL_IP, Poco::UInt16(DEFAULT_PORT));
-    // Instanciar o http request
-    // Preencher o header do request
-    // Instanciar o html form
-    // Preencher o form data
-    // Submeter o form data ao request
-    // Enviar o request
-    // Obter o response
-    // Validar response
-    Poco::Net::SocketAddress socket_address(LOCAL_IP, DEFAULT_PORT);
-    Poco::Net::ServerSocket server_socket(socket_address);
-    Poco::Net::StreamSocket client_socket;
-    client_socket.connect(socket_address);
-    Poco::Net::HTTPServerSession session(server_socket.acceptConnection(), new Poco::Net::HTTPServerParams);
-    Poco::Net::HTTPServerResponseImpl response(session);
-    Poco::Net::HTTPServerRequestImpl request(response, session, new Poco::Net::HTTPServerParams);
-    request.setMethod(Poco::Net::HTTPServerRequest::HTTP_POST);
-    request.setURI("/signature");
-    Poco::Net::HTMLForm form;
-    form.setEncoding(Poco::Net::HTMLForm::ENCODING_MULTIPART);
-    form.addPart("File-To-Assign", new Poco::Net::FilePartSource("doc.txt"));
-    form.addPart("Certificate", new Poco::Net::FilePartSource("certificado_teste_hub.pfx"));
-    form.add("Password", "bry123456");
-    form.prepareSubmit(request);
-    APIRestRequestHandlerFactory factory("upload");
-    std::unique_ptr<Poco::Net::HTTPRequestHandler> handler(factory.createRequestHandler(request));
-    handler->handleRequest(request, response);
-    CPPUNIT_ASSERT_MESSAGE(PRINTF_MESSAGE("Status [%d], Reason [%s]", response.getStatus(), response.getReason().c_str()), response.getStatus() == Poco::Net::HTTPResponse::HTTP_OK);
-    CPPUNIT_ASSERT_MESSAGE(PRINTF_MESSAGE("Content type %s incorreto", response.getContentType().c_str()), response.getContentType() == CONTENT_TYPE_PLAIN_TEXT);
-    CPPUNIT_ASSERT_MESSAGE("Não retornou o conteúdo da assinatura em base 64", response.getContentLength() > 0);
+    Poco::Net::HTTPRequest request_header(Poco::Net::HTTPRequest::HTTP_POST, SIGNATURE_PATH,
+            Poco::Net::HTTPRequest::HTTP_1_1);
+    Poco::Net::HTMLForm form_data;
+    form_data.setEncoding(Poco::Net::HTMLForm::ENCODING_MULTIPART);
+    form_data.addPart(FILE_TO_ASSIGN, new Poco::Net::FilePartSource(FILE_TO_ASSIGN_PATH));
+    form_data.addPart(CERTIFICATE, new Poco::Net::FilePartSource(CERTIFICATE_PATH));
+    form_data.add(PASSWORD, OpenSSLUtils::decrypt_aes_256_cbc(std::getenv(PKCS12_ENVVAR_PASSWORD), AES_KEY,
+            reinterpret_cast<const unsigned char*>(AES_INITIALIZATION_VECTOR)));
+    form_data.prepareSubmit(request_header);
+    std::ostream& request_body = session.sendRequest(request_header);
+    form_data.write(request_body);
+    Poco::Net::HTTPResponse response_header;
+    std::istream& response_body = session.receiveResponse(response_header);
+    CPPUNIT_ASSERT_MESSAGE(PRINTF_MESSAGE("Status [%d], Reason [%s]", response_header.getStatus(),
+            response_header.getReason().c_str()),
+            response_header.getStatus() == Poco::Net::HTTPResponse::HTTP_OK);
+    CPPUNIT_ASSERT_MESSAGE(PRINTF_MESSAGE("Content type %s incorreto",
+            response_header.getContentType().c_str()),
+            response_header.getContentType() == CONTENT_TYPE_PLAIN_TEXT);
+    CPPUNIT_ASSERT_MESSAGE("Não retornou o conteúdo da assinatura em base 64",
+            response_header.getContentLength() > 0);
 }
 
 void APIRestServerTest::teste_rota_verify() {
