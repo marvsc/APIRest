7395674915118f7c20b5aa8d88bcf9f51d2b5e0b
Marcus Chaves <marcus.chaves@digitro.com>
2026-05-22 16:59:29 -0300

    Evolução

--- src/Route/APIRestRequestHandler.cpp
+++ src/Route/APIRestRequestHandler.cpp
@@ -1,9 +1,3 @@
-/*
- * APIRestRequestHandler.cpp
- *
- *  Created on: 27 de jan. de 2026
- *      Author: marcus.chaves
- */
 
 #include "APIRestRequestHandler.h"
 
@@ -13,10 +7,15 @@ namespace Route {
 
 void APIRestRequestHandler::handleRequest(Poco::Net::HTTPServerRequest &request,
         Poco::Net::HTTPServerResponse &response) {
+    // Extrai o caminho da rota
     std::string path(Poco::URI(request.getURI()).getPath());
+
+    // Corrige o caminho da rota.
     if (!path.empty() && path.back() == '/') {
         path.pop_back();
     }
+
+    // Dispara a rotina associada a rota solicitada.
     routes_.at(path)(request, response);
 }
 
--- src/Route/APIRestRequestHandler.h
+++ src/Route/APIRestRequestHandler.h
@@ -1,8 +1,8 @@
 /*
- * APIRestRequestHandler.h
- *
- *  Created on: 27 de jan. de 2026
- *      Author: marcus.chaves
+ * @file APIRestRequestHandler.h
+ * @brief Declaração da classe APIRestRequestHandler, métodos e atributos.
+ * @author Marcus Chaves
+ * @date 2026-01-27
  */
 
 #ifndef SRC_ROUTE_APIRESTREQUESTHANDLER_H_
@@ -17,23 +17,55 @@
 
 namespace Route {
 
+/*
+ * @class APIRestRequestHandler
+ * @brief Classe para manipular requisições recebidas pelo servidor.
+ */
 class APIRestRequestHandler: public Poco::Net::HTTPRequestHandler {
 public:
+
+    /*
+     * @brief Constrói a classe de forma simples.
+     */
     APIRestRequestHandler() {}
+
+    /*
+     * @brief Constrói a classe mapeando as rotas.
+     *
+     * @param[in] routes Mapa de rotas.
+     */
     APIRestRequestHandler(std::map<std::string, std::function<void(Poco::Net::HTTPServerRequest&,
             Poco::Net::HTTPServerResponse&)>> routes) : routes_(routes) {}
+
+    /*
+     * @brief Destrói a classe limpando as rotas.
+     */
     virtual ~APIRestRequestHandler() {
         routes_.clear();
     }
+
+    /*
+     * @brief Adiciona rota
+     *
+     * @param[in] rota Caminho da rota.
+     * @param[in] callback Função chamada ao acessar a rota.
+     */
     void add_route(const std::string &route, std::function<void(Poco::Net::HTTPServerRequest&,
             Poco::Net::HTTPServerResponse&)> callback) {
         routes_.emplace(route, callback);
     }
+
+    /*
+     * @brief Manipula a requisição recebida pelo servidor.
+     *
+     * @param[in] request Requisição recebida pelo servidor.
+     * @param[in] response Resposta a ser enviada pelo servidor.
+     */
     void handleRequest(Poco::Net::HTTPServerRequest &request,
             Poco::Net::HTTPServerResponse &response) override;
 private:
     std::map<std::string, std::function<void(Poco::Net::HTTPServerRequest&,
-            Poco::Net::HTTPServerResponse&)>> routes_;
+            Poco::Net::HTTPServerResponse&)>> routes_; ///< @brief Rotas expostas pelo servidor.
 };
 
 } /* namespace Route */
